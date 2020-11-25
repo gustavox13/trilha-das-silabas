165843935dd97ba83dc288f4366308c076a29c79
gustavox13 <gurodrigues1300@gmail.com>
2020-11-25 15:01:29 -0300

    add principal code

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,201 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] words = new GameObject[8];
+
+    private int currentWord = 0;
+
+    private int currentCorrectID;
+
+    public int QuantPlays = 0;
+
+    [SerializeField]
+    private GameObject kitAnswer1;
+
+    [SerializeField]
+    private GameObject kitAnswer2;
+
+
+    
+
+    [SerializeField]
+    private GameObject Adelaide;
+
+    private Animator adelaideAnim;
+
+    private AudioSource adelaideSom;
+
+    private string currentdirection;
+
+
+
+    [SerializeField]
+    private GameObject scenary;
+
+    private Animator scenaryAnim;
+
+    [SerializeField]
+    private GameObject school;
+
+    private Animator schoolAnim;
+
+    private void Awake()
+    {
+        ShuffleQuestions();
+        adelaideAnim = Adelaide.GetComponent<Animator>();
+        adelaideSom = Adelaide.GetComponent<AudioSource>();
+        scenaryAnim = scenary.GetComponent<Animator>();
+        schoolAnim = school.GetComponent<Animator>();
+    }
+
+
+    private void Start()
+    {
+        StartTurn();
+    }
+
+
+
+    //INICIA O TURNO
+    private void StartTurn()
+    {
+        words[currentWord].SetActive(true);
+
+        if(words[currentWord].GetComponent<WordClass>().CorrectID == 1 || words[currentWord].GetComponent<WordClass>().CorrectID == 2) //SELECIONA E ATIVA O KIT DE RESPOSTAS
+        {
+            kitAnswer1.SetActive(true);
+        }
+        else
+        {
+            kitAnswer2.SetActive(true);
+        }
+    }
+
+    //VERIFICA A RESPOSTA
+    public void CheckAnswer(int selectedAnswer)
+    {
+        QuantPlays++;
+
+        if (selectedAnswer == words[currentWord].GetComponent<WordClass>().CorrectID)
+        {
+            StartCoroutine(CorrectAnswer());
+        }
+        else
+        {
+            StartCoroutine(WrongAnswer());
+        }
+    }
+
+    
+    public void getDirection(string direction)
+    {
+
+        currentdirection = direction;
+        Debug.Log(currentdirection);
+    }
+    
+
+    IEnumerator WrongAnswer()
+    {
+
+        if (words[currentWord].GetComponent<WordClass>().CorrectID == 1 || words[currentWord].GetComponent<WordClass>().CorrectID == 2) //SELECIONA E DESATIVA O KIT DE RESPOSTAS
+        {
+            kitAnswer1.SetActive(false);
+        }
+        else
+        {
+            kitAnswer2.SetActive(false);
+        }
+
+
+        adelaideSom.Play();
+        adelaideAnim.SetTrigger("wrong");
+
+        yield return new WaitForSeconds(1f);
+
+        StartTurn();
+    }
+
+
+    IEnumerator CorrectAnswer()
+    {
+
+        if (words[currentWord].GetComponent<WordClass>().CorrectID == 1 || words[currentWord].GetComponent<WordClass>().CorrectID == 2) //SELECIONA E DESATIVA O KIT DE RESPOSTAS
+        {
+            kitAnswer1.SetActive(false);
+        }
+        else
+        {
+            kitAnswer2.SetActive(false);
+        }
+
+        words[currentWord].GetComponent<WordClass>().EditWord();
+
+
+        yield return new WaitForSeconds(1.2f);
+
+        adelaideAnim.SetTrigger(currentdirection);
+
+
+        yield return new WaitForSeconds(1.6f);
+
+        schoolAnim.SetTrigger("aument");
+        scenaryAnim.SetTrigger("advance");
+        
+
+        yield return new WaitForSeconds(0.5f);
+
+        words[currentWord].SetActive(false);
+
+        FinalStep();
+    }
+
+
+    private void FinalStep()
+    {
+        if(currentWord < 7)
+        {
+            currentWord++;
+            StartTurn();
+        }
+        else
+        {
+            Debug.Log("fim do jogo");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //EMBARALHAR FRASES
+    private void ShuffleQuestions()
+    {
+        for (int i = 0; i < words.Length; i++)
+        {
+            GameObject obj = words[i];
+            int randomizeArray = Random.Range(0, i);
+            words[i] = words[randomizeArray];
+            words[randomizeArray] = obj;
+        }
+    }
+}
---
+++ Assets/Scripts/WordClass.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordClass : MonoBehaviour
+{
+
+    public int CorrectID;
+
+    private Text myText;
+
+    [SerializeField]
+    private string editedWord;
+
+    [SerializeField]
+    private GameObject audioCorrect;
+
+    private AudioSource correctAlert;
+
+
+    private void Start()
+    {
+        correctAlert = audioCorrect.GetComponent<AudioSource>();
+        myText = this.gameObject.GetComponent<Text>();        
+    }
+
+
+
+    public void EditWord()
+    {
+        correctAlert.Play();
+        myText.text = editedWord;
+    }
+
+
+
+
+}
